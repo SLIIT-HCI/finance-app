c54512f7ecff4b38662c15d1b3b29ce1acb5c009
ashi1015 <ayeshpramo@gmail.com>
2021-02-25 22:21:55 +0530

    split button implementation test

--- app/src/main/java/com/example/financemanagementapp/Split_Expenses.java
+++ app/src/main/java/com/example/financemanagementapp/Split_Expenses.java
@@ -3,15 +3,20 @@ package com.example.financemanagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class Split_Expenses extends AppCompatActivity {
 
+    //Global variables
     Button splitButton;
     EditText totalAmount, No_Members;
     TextView amountPerMember;
+
+    int Nmembers;
+    double total, pPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,34 @@ public class Split_Expenses extends AppCompatActivity {
         No_Members = (EditText) findViewById(R.id.noOfMembers);
         amountPerMember = (TextView) findViewById(R.id.perMemberTextView);
 
-//        String totAmount, noMemb, amountPMemeb;
+        splitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                float TotAmount = Float.parseFloat(totalAmount.getText() + "");
+                int noOfMembers = Integer.parseInt(No_Members.getText() + "");
+                float result = ((float) TotAmount / noOfMembers);
+                amountPerMember.setText((int) result);
+
+
+//                String TotAmount = totalAmount.getText().toString();
+//                String noOfMembers = No_Members.getText().toString();
+//                double result = ((double) TotAmount/noOfMembers);
+//                amountPerMember.setText(result);
+
+//                buttonDivision.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        mValueOne = Float.parseFloat(calcAns.getText() + "");
+//                        division = true;
+//                        calcAns.setText(null);
+//                    }
+//                });
+//                if (division == true) {
+//                    calcAns.setText(mValueOne / mValueTwo + "");
+//                    division = false;
+//                }
+            }
+        });
     }
 }
