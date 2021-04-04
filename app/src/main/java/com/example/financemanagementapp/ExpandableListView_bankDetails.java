e22810abbe99ad1db21a489a4f45563ec2478675
ashi1015 <ayeshpramo@gmail.com>
2021-04-04 21:16:02 +0530

    Adding Floating action buttons Bank Details function usnder user profile

---
+++ app/src/main/java/com/example/financemanagementapp/ExpandableListView_bankDetails.java
@@ -0,0 +1,99 @@
+package com.example.financemanagementapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExpandableListView_bankDetails extends AppCompatActivity {
+
+    List<String> groupList;
+    List<String> childList;
+    Map<String, List<String>> AccountCollection;
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expandable_list_view_bank_details);
+
+        createGroupList();
+        createCollection();
+
+        expandableListView = findViewById(R.id.expandableLV_Bank);
+        expandableListAdapter = new BankExpandibleListAdapter(this, groupList, AccountCollection);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            //declaring the last expanded position as -1
+            int lastExpandiblePosition = -1;
+            //collapsing the previously expanded Item.
+            @Override
+            public void onGroupExpand(int i) {
+                if(lastExpandiblePosition != -1 && i != lastExpandiblePosition){
+                    expandableListView.collapseGroup(lastExpandiblePosition);
+                }
+                lastExpandiblePosition = i;
+            }
+        });
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+                String selected = expandableListAdapter.getChild(i, i1).toString();
+                Toast.makeText(getApplicationContext(), "Selected" + selected, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void createCollection() {
+        String [] Account1Model = {"test1", "test2", "test3"};
+        String [] Account2Model = {"test1", "test2", "test3"};
+        String [] Account3Model = {"test1", "test2", "test3"};
+        String [] Account4Model = {"test1", "test2", "test3"};
+        String [] Account5Model = {"test1", "test2", "test3"};
+
+        AccountCollection = new HashMap<String, List<String>>();
+
+        for(String group : groupList){
+            if(group.equals("Account1")){
+                loadChild(Account1Model);
+            }else if(group.equals("Account2")){
+                loadChild(Account2Model);
+            }else if(group.equals("Account3")){
+                loadChild(Account3Model);
+            }else if(group.equals("Account4")) {
+                loadChild(Account4Model);
+            }else if(group.equals("Account4")){
+                loadChild(Account5Model);
+            }
+            AccountCollection.put(group, childList);
+        }
+    }
+
+    private void loadChild(String[] accountModel) {
+        childList = new ArrayList<>();
+        for(String model : accountModel){
+            childList.add(model);
+        }
+    }
+
+    private void createGroupList() {
+        groupList = new ArrayList<>();
+        groupList.add("Account1");
+        groupList.add("Account2");
+        groupList.add("Account3");
+        groupList.add("Account4");
+        groupList.add("Account5");
+
+    }
+}
--- app/src/main/java/com/example/financemanagementapp/UserProfile.java
+++ app/src/main/java/com/example/financemanagementapp/UserProfile.java
@@ -1,14 +1,143 @@
 package com.example.financemanagementapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.service.controls.actions.FloatAction;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserProfile extends AppCompatActivity {
+
+    private final int REQUEST_CODE=10;
+    ImageView selectFromGallery;
+    TextView change_password, mobile1, mobile2, home;
+    TextView bankDetails;
+
+    //private FirebaseAuth firebaseAuth1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        //getting firebase auth object
+        //firebaseAuth1 = FirebaseAuth.getInstance();
+
+        selectFromGallery = (ImageView) findViewById(R.id.selectFromGallery);
+        change_password = (TextView)findViewById(R.id.changePassword);
+        mobile1 = (TextView) findViewById(R.id.mobile1);
+        mobile2 = (TextView) findViewById(R.id.mobile2);
+        home = (TextView) findViewById(R.id.mobileHome);
+        bankDetails = (TextView) findViewById(R.id.TVBankDetail);
+
+        selectFromGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent selectImage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(selectImage,REQUEST_CODE);
+                //selectImage.setText("Select Again");
+            }
+        });
+
+        change_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ChangePassword = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
+                startActivity(ChangePassword);
+            }
+        });
+
+        mobile1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                enterMobileNumber();
+            }
+        });
+
+        mobile2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enterMobileNumber();
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            enterMobileNumber();
+        }
+    });
+
+        bankDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bank = new Intent(getApplicationContext(), ExpandableListView_bankDetails.class);
+                startActivity(bank);
+            }
+        });
+
     }
+
+
+
+    private void enterMobileNumber(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Phone Number");
+        //alert.setMessage("Message");
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                // Do something with value!
+            }
+        });
+
+        alert.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Delete phone number
+            }
+        });
+
+        alert.show();
+    }
+
+//    private void enterEmail(){
+//        AlertDialog.Builder emailAlert = new AlertDialog.Builder(this);
+//        emailAlert.setTitle("Change email address");
+//
+//        final EditText enterEmail = new EditText(this);
+//        emailAlert.setView(enterEmail);
+//
+//        emailAlert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                Intent intent = new Intent(getApplicationContext(), EmailVerificationActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        emailAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                //cancel
+//            }
+//        });
+//    }
 }
